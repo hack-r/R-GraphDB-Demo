f37d8719def93b5e602c3d1511662191f3076a55
Jason Miller <jmillerlaureate@gmail.com>
2014-09-24 14:40:53 -0400

    Initial commit

---
+++ graphdb.R
@@ -0,0 +1,6 @@
+## File: graphdb.R
+## Description: A demonstration of how to create and use graph DB's in R with Big Data
+## Copyright: (c) 2014, Jason D. Miller, MS, MS
+## http://hack-r.com 
+## http://hack-r.github.io
+## http://github.com/hack-r
---
+++ graphdb_functions.R
@@ -0,0 +1,6 @@
+## File: graphdb_functions.R
+## Description: Functions/packages use in graphdb.R
+## Copyright: (c) 2014, Jason D. Miller, MS, MS
+## http://hack-r.com 
+## http://hack-r.github.io
+## http://github.com/hack-r
