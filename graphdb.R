3a84e38437da2062c5f0339a13e5da63d256d772
Jason Miller <jmillerlaureate@gmail.com>
2014-09-25 16:30:12 -0400

    .

--- graphdb.R
+++ graphdb.R
@@ -37,8 +37,40 @@ doc.corpus <- tm_map(doc.corpus, content_transformer(removeWords),
 doc.corpus <- tm_map(doc.corpus, content_transformer(removeWords), 
                      stopwords("english"))
 #Create the TDM
-doc.tdm <- TermDocumentMatrix(doc.corpus)
+doc.tdm   <- TermDocumentMatrix(doc.corpus)
+notsparse <- 
 
 # Initialize a Neo4J Graph Database ---------------------------------------
 hack_r = startGraph("http://localhost:7474/db/data/") 
+
+
+# Utilize Transactional Endpoint ------------------------------------------
+appendCypher()
+
+query = "
+MERGE (liker:User {name:{liker_id}})
+MERGE (author:User {name:{author_id}})
+CREATE (liker)<-[:LIKER]-(:liked_objectID {number:{liked_objectID}})-[:AUTHOR]->(author)"
+
+t = newTransaction(hack_r)
+
+for (i in 1:nrow(Z)) {
+  liker_id = Z[i, ]$liker
+  author_id = Z[i, ]$author
+  liked_objectID = Z[i, ]$liked_objectID
+  
+  appendCypher(t, 
+               query, 
+               liker_id = liker_id, 
+               author_id = author_id, 
+               liked_objectID = liked_objectID)
+}
+
+commit(t)
+
+cypher(hack_r, "MATCH (l:User)<-[:LIKER]-(i:liked_objectID)-[:AUTHOR]->(a:User)
+               RETURN l.name, i.number, a.name")
+# It works!
+
+summary(hack_r)
 
